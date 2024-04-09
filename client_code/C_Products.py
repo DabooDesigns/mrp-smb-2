5c6d4ef30bc22307345d85828640fb2fca9ec7e7
david@daboodesigns.com <david@daboodesigns.com>
2024-04-09 00:19:23 +0000

    Edited C_Products

--- client_code/C_Products.py
+++ client_code/C_Products.py
@@ -13,3 +13,30 @@ from anvil.tables import app_tables
 
 def say_hello():
   print("Hello, world")
+
+  def file_loader_1_change(self, file, **event_args):
+    for fl in self.file_loader_1.files:
+      fl1 = self.Image_Resizer_change(fl)
+      app_tables.images.add_row(image=fl1)
+
+# May need to create another variable as a way to send the user back to a page when the file load operation has been completed.
+#    open_form('Form1')
+
+# Resizes the uploaded image(s) resizes to 480w and initiates a download and stores a 1200w in the product_images table
+  def Image_Resizer_change(self, file, **event_args):
+    filename = file.name
+#    tfile = anvil.image.generate_thumbnail(file, 120) 
+    mfile = anvil.image.generate_thumbnail(file, 480) 
+    lfile = anvil.image.generate_thumbnail(file, 1200) 
+
+    tstrings = ["thumbnail", filename]
+    tfilename = "-".join(tstrings)
+    mstrings = ["m", filename]
+    mfilename = "-".join(mstrings)    
+    lstrings = ["l", filename]
+    lfilename = "-".join(lstrings)    
+
+    image_medium = anvil.server.call("name_image",mfile,mfilename)
+    anvil.media.download(image_medium)  
+    image_large = anvil.server.call("name_image",lfile,lfilename)    
+    return image_large
